aa078fcf849433d56fe764804dbb6c120c6962fa
Watcher1986 <deniss2007@gmail.com>
2023-06-12 16:25:05 +0300

    fix: mobile nav title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
           />
           <p className='text-white font-bold text-[18px] cursor-pointer flex'>
             Denis &nbsp;
-            <span className='sm:block hidden'>| Sokolenko</span>
+            <span>| Sokolenko</span>
           </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
